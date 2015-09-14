507366aef86564ce2f5c54cd896adeb9ce8ba5d3
c0647063 <c0647063@SB-N208-23.lambton.on.ca>
2015-09-14 15:48:38 -0400

    Initial Commit

---
+++ src/javaas12015/JavaAs12015.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaas12015;
+
+/**
+ *
+ * @author c0647063
+ */
+public class JavaAs12015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /**************
+         * 
+         * Job Posting: Software Engineer II.
+         * http://www.cdkjobs.com/jobs/technology/london-on-can/software-engineer-ii/82366?SID=64&utm_source=linkedin&utm_medium=referral&utm_campaign=ongig-job-aggregators&SID=231&utm_source=indeed&utm_medium=referral&utm_campaign=ongig-job-aggregators&rx_job=46865206&rx_source=Indeed&rx_campaign=Indeed126         * 
+         * 
+         * JAVA - https://en.wikipedia.org/wiki/Java_(programming_language)
+         * SPRING JS - http://docs.spring.io/spring-webflow/docs/current-SNAPSHOT/reference/html/spring-js.html
+         * AngularJS - https://en.wikipedia.org/wiki/AngularJS
+         * 
+         * Toronto - AVG $77,208 per anum - http://swz.salary.com/CanadaSalaryWizard/Level-II-Software-Engineer-Salary-Details-Toronto-ON.aspx
+         * Waterloo - AVG $79,216 per anum - http://swz.salary.com/CanadaSalaryWizard/Level-II-Software-Engineer-Salary-Details-Waterloo-ON.aspx
+         * Vancouver - AVG $81,671 per anum - http://swz.salary.com/CanadaSalaryWizard/Level-II-Software-Engineer-Salary-Details-Vancouver-BC.aspx
+         */
+        
+        
+        
+    }
+    
+}
